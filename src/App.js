f2df8aecdf4311657379b1758ec3b9883a0d5a82
Arslan Ahmed <arslanahmed.kcsf18@iba-suk.edu.pk>
2022-12-03 11:57:00 +0530

    footer component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Analytics from "./components/Analytics";
 import Cards from "./components/Cards";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Newsletter from "./components/Newsletter";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Analytics />
       <Newsletter />
       <Cards />
+      <Footer></Footer>
     </div>
   );
 }
